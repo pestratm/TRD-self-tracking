9adf7d0fdc0b1049046d3b48574593eb55c667f8
Alexander Schmah <alexander.schmah@cern.ch>
2020-05-06 19:00:08 +0200

    Fixed sign bug in vector projection calculate_point_on_Straight_dca_to_Point

--- TRD_ST_Analyze_tracklets.h
+++ TRD_ST_Analyze_tracklets.h
@@ -19,8 +19,12 @@ private:
 
     TString HistName;
 
+    TEveLine* TEveLine_beam_axis = NULL;
     TEveLine* TPL3D_helix = NULL;
     vector<TEveLine*> vec_TPL3D_helix;
+    vector< vector<TEveLine*> > vec_TEveLine_tracklets;
+    Int_t N_tracklets_layers[6] = {0};
+    Int_t color_layer[6] = {kBlue,kGreen,kOrange,kCyan,kMagenta,kYellow};
 
 public:
     Ali_TRD_ST_Analyze();
@@ -39,7 +43,19 @@ public:
 Ali_TRD_ST_Analyze::Ali_TRD_ST_Analyze()
 {
     // constructor
+    TEveManager::Create();
+
     TPL3D_helix = new TEveLine();
+    TEveLine_beam_axis = new TEveLine();
+    TEveLine_beam_axis ->SetNextPoint(0.0,0.0,-300.0);
+    TEveLine_beam_axis ->SetNextPoint(0.0,0.0,300.0);
+    TEveLine_beam_axis ->SetName("beam axis");
+    TEveLine_beam_axis ->SetLineStyle(1);
+    TEveLine_beam_axis ->SetLineWidth(4);
+    TEveLine_beam_axis ->SetMainColor(kBlue);
+    gEve->AddElement(TEveLine_beam_axis);
+
+    vec_TEveLine_tracklets.resize(6); // layers
 }
 //----------------------------------------------------------------------------------------
 
@@ -161,6 +177,13 @@ void Ali_TRD_ST_Analyze::Loop_event(Long64_t i_event)
         TV3_offset      = TRD_ST_Tracklet ->get_TV3_offset();
         TV3_dir         = TRD_ST_Tracklet ->get_TV3_dir();
         i_det           = TRD_ST_Tracklet ->get_TRD_det();
+
+        //create "tracklets"
+        Int_t i_sector = (Int_t)(i_det/30);
+        Int_t i_stack  = (Int_t)(i_det%30/6);
+        Int_t i_layer  = i_det%6;
+        //Int_t i_det = layer + 6*stack + 30*sector;
+
     }
     //--------------------------------------------------
 
@@ -175,8 +198,6 @@ void Ali_TRD_ST_Analyze::Loop_event(Long64_t i_event)
 void Ali_TRD_ST_Analyze::Draw_event(Long64_t i_event)
 {
     printf("Ali_TRD_ST_Analyze::Draw_event \n");
-
-    TEveManager::Create();
 
     if (!input_SE->GetEntry( i_event )) return 0; // take the event -> information is stored in event
 
@@ -199,30 +220,34 @@ void Ali_TRD_ST_Analyze::Draw_event(Long64_t i_event)
     {
         TRD_ST_TPC_Track = TRD_ST_Event ->getTrack(i_track);
 
-        TPL3D_helix ->Clear();
-        HistName = "track ";
-        HistName += i_track;
-        TPL3D_helix ->SetName(HistName.Data());
+        vec_TPL3D_helix.resize(i_track+1);
+        vec_TPL3D_helix[i_track] = new TEveLine();
 
         for(Double_t track_path = 0.0; track_path < 300; track_path += 1.0)
         {
             TRD_ST_TPC_Track ->Evaluate(track_path,track_pos);
-            TPL3D_helix ->SetNextPoint(track_pos[0],track_pos[1],track_pos[2]);
+            vec_TPL3D_helix[i_track] ->SetNextPoint(track_pos[0],track_pos[1],track_pos[2]);
+
+            //if(i_track == 0) printf("track_path: %4.3f, pos: {%4.2f, %4.2f, %4.2f} \n",track_path,track_pos[0],track_pos[1],track_pos[2]);
         }
 
-        TPL3D_helix ->SetLineStyle(1);
-        TPL3D_helix ->SetLineColor(kRed);
-        TPL3D_helix ->SetLineWidth(3);
-        gEve        ->AddElement(TPL3D_helix);
+        HistName = "track ";
+        HistName += i_track;
+        vec_TPL3D_helix[i_track]    ->SetName(HistName.Data());
+        vec_TPL3D_helix[i_track]    ->SetLineStyle(1);
+        vec_TPL3D_helix[i_track]    ->SetLineWidth(2);
+        vec_TPL3D_helix[i_track]    ->SetMainColor(kRed);
+        gEve->AddElement(vec_TPL3D_helix[i_track]);
     }
 
-    gEve->Redraw3D(kTRUE);
     //--------------------------------------------------
 
 
 
     //--------------------------------------------------
     // TRD tracklet loop
+    Double_t scale_length = 3.0;
+
     TVector3 TV3_offset;
     TVector3 TV3_dir;
     Int_t    i_det;
@@ -233,7 +258,36 @@ void Ali_TRD_ST_Analyze::Draw_event(Long64_t i_event)
         TV3_offset      = TRD_ST_Tracklet ->get_TV3_offset();
         TV3_dir         = TRD_ST_Tracklet ->get_TV3_dir();
         i_det           = TRD_ST_Tracklet ->get_TRD_det();
+
+        if(TV3_offset.Mag() > 1000.0) continue;
+
+        //create "tracklets"
+        Int_t i_sector = (Int_t)(i_det/30);
+        Int_t i_stack  = (Int_t)(i_det%30/6);
+        Int_t i_layer  = i_det%6;
+        //Int_t i_det = layer + 6*stack + 30*sector;
+
+        vec_TEveLine_tracklets[i_layer].resize(N_tracklets_layers[i_layer]+1);
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]] = new TEveLine();
+
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]] ->SetNextPoint(TV3_offset[0],TV3_offset[1],TV3_offset[2]);
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]] ->SetNextPoint(TV3_offset[0] + scale_length*TV3_dir[0],TV3_offset[1] + scale_length*TV3_dir[1],TV3_offset[2] + scale_length*TV3_dir[2]);
+
+        Double_t radius = TMath::Sqrt( TMath::Power(TV3_offset[0],2) + TMath::Power(TV3_offset[1],2) );
+        printf("i_tracklet: %d, radius: %4.3f, pos A: {%4.2f, %4.2f, %4.2f}, pos B: {%4.2f, %4.2f, %4.2f} \n",i_tracklet,radius,TV3_offset[0],TV3_offset[1],TV3_offset[2],TV3_offset[0] + scale_length*TV3_dir[0],TV3_offset[1] + scale_length*TV3_dir[1],TV3_offset[2] + scale_length*TV3_dir[2]);
+
+        HistName = "tracklet ";
+        HistName += i_tracklet;
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]]    ->SetName(HistName.Data());
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]]    ->SetLineStyle(1);
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]]    ->SetLineWidth(2);
+        vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]]    ->SetMainColor(color_layer[i_layer]);
+        gEve->AddElement(vec_TEveLine_tracklets[i_layer][N_tracklets_layers[i_layer]]);
+
+        N_tracklets_layers[i_layer]++;
     }
+
+    gEve->Redraw3D(kTRUE);
     //--------------------------------------------------
 }
 //----------------------------------------------------------------------------------------
