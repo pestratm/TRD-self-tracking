8a1699dd2067088e2579f4ef8914f195db39898a
Alexander Schmah <alexander.schmah@cern.ch>
2020-05-26 09:45:56 +0200

    Modified QC of damaged chamnbers to new flag ranging from 0..7

--- TRD_ST_Analyze_tracklets.cc
+++ TRD_ST_Analyze_tracklets.cc
@@ -5,9 +5,9 @@ void TRD_ST_Analyze_tracklets()
     printf("TRD_ST_Analyze_tracklets started \n");
 
     Ali_TRD_ST_Analyze*  TRD_ST_Analyze = new Ali_TRD_ST_Analyze();
-    TRD_ST_Analyze ->Init_tree("List_data_ADC.txt");
+    TRD_ST_Analyze ->Init_tree("List_data.txt");
 
-    Long64_t event = 3;
+    Long64_t event = 4;
     TRD_ST_Analyze ->Loop_event(event);
     TRD_ST_Analyze ->Draw_event(event);
     TRD_ST_Analyze ->Do_TPC_TRD_matching(event,3.0,10.0);
