9063c9d5de5c559401657326b86ba902d300ddde
Alexander Schmah <alexander.schmah@cern.ch>
2020-07-24 13:44:04 +0200

    Kalman tracker changed, 3 tracklets accepted, don't go to layer 5

--- drawhists.cc
+++ drawhists.cc
@@ -2,9 +2,11 @@
 R__LOAD_LIBRARY(TRD_Kalman_Tracking_cxx.so);
 R__LOAD_LIBRARY(TRD_ST_Analyze_tracklets_cxx.so);
 
+// Environment variables
 //#define ENV_PI
 #define ENV_ALEX
 //#define ENV_PI_SVEN
+
 void drawhists(TString input_list = "List_data_ADC.txt")
 {
 
