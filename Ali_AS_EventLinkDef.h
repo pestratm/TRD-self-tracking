586f4e74f2341f3b2af54ac3d84d64edd6a740d4
Alexander Schmah <alexander.schmah@cern.ch>
2020-11-11 14:58:50 +0100

    Added "+"

--- Ali_AS_EventLinkDef.h
+++ Ali_AS_EventLinkDef.h
@@ -1,11 +1,11 @@
 #ifdef __CINT__
 
 #pragma link C++ nestedclass;
-#pragma link C++ class Ali_AS_TRD_digit;
-#pragma link C++ class Ali_AS_Track;
-#pragma link C++ class Ali_AS_Tracklet;
-#pragma link C++ class Ali_AS_offline_Tracklet;
-#pragma link C++ class Ali_AS_Event;
+#pragma link C++ class Ali_AS_TRD_digit+;
+#pragma link C++ class Ali_AS_Track+;
+#pragma link C++ class Ali_AS_Tracklet+;
+#pragma link C++ class Ali_AS_offline_Tracklet+;
+#pragma link C++ class Ali_AS_Event+;
 
 
 #endif
--- Ali_TRD_ST_LinkDef.h
+++ Ali_TRD_ST_LinkDef.h
@@ -1,10 +1,10 @@
 #ifdef __CINT__
 
 #pragma link C++ nestedclass;
-#pragma link C++ class Ali_TRD_ST_Tracklets;
-#pragma link C++ class Ali_TRD_ST_TPC_Track;
-#pragma link C++ class Ali_TRD_ST_Event;
-#pragma link C++ class Ali_Helix;
+#pragma link C++ class Ali_TRD_ST_Tracklets+;
+#pragma link C++ class Ali_TRD_ST_TPC_Track+;
+#pragma link C++ class Ali_TRD_ST_Event+;
+#pragma link C++ class Ali_Helix+;
 
 
 #endif
